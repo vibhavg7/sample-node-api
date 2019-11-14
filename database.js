a1cee0196f73a0c0fad424ed873b1194af08a477
Vibhav Gupta <vibhav.gupta@nagarro.com>
2019-11-14 18:13:41 +0530

    cc

---
+++ database.js
@@ -0,0 +1,31 @@
+var mysql = require('mysql');
+
+var grostepawsdb_config = {
+    connectionLimit: 1000,
+    host: 'vibhavg91.cce5kiug4ajr.us-east-2.rds.amazonaws.com',
+    user: 'root',
+    password: 'password',
+    database: 'grostep'
+};
+
+
+export async function connect_aws_grostep() {
+
+    var pool = mysql.createPool(grostepawsdb_config);
+    await pool.getConnection((err, connection) => {
+        if (err) {
+            if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+                console.error('Database connection was closed.')
+            }
+            if (err.code === 'ER_CON_COUNT_ERROR') {
+                console.error('Database has too many connections.')
+            }
+            if (err.code === 'ECONNREFUSED') {
+                console.error('Database connection was refused.')
+            }
+        }
+        if (connection) connection.release()
+        return;
+    });
+    return pool;
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+// default route
+app.get('/', function (req, res) {
+    return res.send({ error: true, message: 'hello',keys:process.env });
+});
+
+ // set port
+ app.listen(3000, function () {
+    console.log('Node app is running on port 3000');
+});
+module.exports = app;
