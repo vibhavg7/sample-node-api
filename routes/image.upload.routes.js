490cbb7f7f8692fa672c243690b1377c05dc9daf
Vibhav Gupta <vibhav.gupta@nagarro.com>
2019-11-16 14:08:45 +0530

    updated changes

--- routes/image.upload.routes.js
+++ routes/image.upload.routes.js
@@ -3,34 +3,35 @@ var router = express.Router();
 var uploadImage = require("../services/file-upload");
 const singleUpload = uploadImage.single('image');
 var productsController = require("../controllers/products.controller");
-// var categoryController = require("../controllers/category.controller");
-// var storesController = require("../controllers/stores.controller");
+var categoryController = require("../controllers/category.controller");
+var storesController = require("../controllers/stores.controller");
 
-router.post('/products/:productId',function(req, res, next) {
-        singleUpload(req, res, function (err) {
-        if (err) {            
-            return res.status(422).send({ errors: [{ title: 'File Upload Error',key1:process.env.SECRET_ACCESS_KEY,key3:process.env, detail: err.message }] });
+router.post('/products/:productId', function (req, res, next) {
+    singleUpload(req, res, function (err) {
+        if (err) {
+            return res.status(422).send({ errors: [{ title: 'File Upload Error', detail: err.message }] });
         }
         productsController.updateProductImages(req.params.productId, req.file.location, req, res);
-    })  
+    })
 });
 
 
+router.post('/category/:categoryid', function (req, res, next) {
+    singleUpload(req, res, function (err) {
+        if (err) {
+            return res.status(422).send({ errors: [{ title: 'File Upload Error', detail: err.message }] });
+        }
+        categoryController.updateStoreCategoryImages(req.params.categoryid, req.file.location, req, res);
+    })
+});
 
-
-
-
-
-
-// app.post('/products/:productId', (req, res) => {
-//     singleUpload(req, res, function (err) {
-//         if (err) {            
-//             return res.status(422).send({ errors: [{ title: 'File Upload Error',key:process.env.secretAccessKey,key2:process.env.accessKeyId,key3:process.env.region, detail: err.message }] });
-//         }
-//         productsController.updateProductImages(req.params.productId, req.file.location, req, res);
-//     })
-// });
-
-
+router.post('/merchants/:merchantId', function (req, res, next) {
+    singleUpload(req, res, function (err) {
+        if (err) {
+            return res.status(422).send({ errors: [{ title: 'File Upload Error', detail: err.message }] });
+        }
+        storesController.updateStoreImages(req.params.merchantId, req.file.location, req, res);
+    })
+});
 
 module.exports = router;
