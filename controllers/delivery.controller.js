2d36ab878b8c60f40680504737878c34b581d975
Vibhav Gupta <vibhav.gupta@nagarro.com>
2020-01-30 18:27:41 +0530

    cc

--- controllers/delivery.controller.js
+++ controllers/delivery.controller.js
@@ -12,7 +12,7 @@ var pool = mysql.createPool({
 
 exports.fetchAllNewOrders = function(req,res) {
     pool.getConnection(function (err, dbConn) {
-        dbConn.query("SELECT * FROM orders where delivery_person_id = 0 and order_deliveryperson_status = 1", function (err, orders) {
+        dbConn.query("SELECT o.order_id,o.total_amount,o.delivery_fee,o.discount_amount,o.payable_amount,o.status AS 'order_current_status',o.total_item_count,o.deliver_now,o.delivery_date,o.delivery_slot,o.instructions,o.order_deliveryperson_status,cda.customer_name,cda.phone AS 'customer_phone_number',cda.flatNumber AS 'customer_flatNumber',cda.landmark AS 'customer_landmark',cda.longitude AS 'customer_longitude',cda.latitude AS 'customer_latitude',cda.pincode AS 'customer_pincode',cda.city AS 'customer_city',cda.state AS 'customer_state',cda.country AS 'customer_country',cda.address AS 'customer_address',cda.address2 AS 'customer_address2',s.store_name,s.phone_number AS 'store_phone_number', s.alternative_number AS 'store_alternative_number',s.address AS 'store_address',s.state AS 'store_state',s.city AS 'store_city',s.country AS 'store_country', s.pin_code As 'store_pincode',s.latitude AS 'store_latitude', s.longitude AS 'store_longitude',pm.payment_method_name FROM grostep.orders o inner join stores s on o.store_id = s.store_id inner join payment_method pm on o.payment_mode = pm.payment_method_id inner join grostep.customer_delivery_address cda on o.delivery_address_id = cda.delivery_address_id where o.delivery_person_id = 0 and o.order_deliveryperson_status = 1", function (err, orders) {
             if (err) {
                 res.json({
                     status: 400,
